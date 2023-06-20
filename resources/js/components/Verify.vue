7fef7731edf2c17b78bf9882f06abb59334627a4
eavlongsok <esok@paragoniu.edu.kh>
2023-06-20 16:04:42 +0700

    changed the address for verify.vue

--- resources/js/components/Verify.vue
+++ resources/js/components/Verify.vue
@@ -5,8 +5,22 @@
             <div class="m-3">
                 <label>Business Name</label>
                 <input name="business_name" type="text" class="border-2 pl-2 rounded-md py-1 my-2 focus:outline w-full" v-model="businessName">
-                <label>Business Address</label>
-                <input name="business_add" type="text" class="border-2 pl-2 rounded-md py-1 my-2 focus:outline w-full" v-model="businessAdd">
+
+                <label>Building Number</label>
+                <input name="building_number" type="text" class="border-2 pl-2 rounded-md py-1 my-2 focus:outline w-full" v-model="buildingNumber">
+
+                <label>Street Number</label>
+                <input name="street_number" type="text" class="border-2 pl-2 rounded-md py-1 my-2 focus:outline w-full" v-model="streetNumber">
+
+                <label>City</label>
+                <input name="city" type="text" class="border-2 pl-2 rounded-md py-1 my-2 focus:outline w-full" v-model="city">
+
+                <label>Province</label>
+                <input name="province" type="text" class="border-2 pl-2 rounded-md py-1 my-2 focus:outline w-full" v-model="province">
+
+                <label>ZIP Code</label>
+                <input name="zip_code" type="text" class="border-2 pl-2 rounded-md py-1 my-2 focus:outline w-full" v-model="zipCode">
+
                 <label class="block my-2 mt-4">Business Description</label>
                 <textarea name="business_info" class="border-2 pl-2 rounded-md py-1 focus:outline w-full h-[8rem]" v-model="businessInfo"></textarea>
                 <div class="flex justify-end">
@@ -17,7 +31,7 @@
                     </div>
                 </div>
             </div>
-            
+
         </div>
     </div>
 </template>
@@ -28,19 +42,27 @@
         data(){
             return {
                 businessName: '',
-                businessAdd: '',
+                buildingNumber: '',
+                streetNumber: '',
+                city: '',
+                province: '',
+                zipCode: '',
                 businessInfo: '',
-            }  
+            }
         },
         methods: {
             async submit(){
                 try{
                     let verifyData = new FormData()
                     verifyData.append('business_name', this.businessName)
-                    verifyData.append('business_add', this.businessAdd)
+                    verifyData.append('building_number', this.buildingNumber)
+                    verifyData.append('street_number', this.streetNumber)
+                    verifyData.append('city', this.city)
+                    verifyData.append('province', this.province)
+                    verifyData.append('zip_code', this.zipCode)
                     verifyData.append('business_info', this.businessInfo)
 
-                    const response = await axios.post('/api/verify', verifyData, 
+                    const response = await axios.post('/api/verify', verifyData,
                     {
                         headers: {
                             Authorization: `Bearer ${localStorage.getItem('seller_token')}`
@@ -51,7 +73,7 @@
                 catch(err){
                     console.log(err.response.data)
                 }
-            }   
+            }
         }
     }
 </script>
