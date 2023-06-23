0fe446dcdfeb84e0feac30b291ff176325222689
eavlongsok <esok@paragoniu.edu.kh>
2023-06-23 23:39:22 +0700

    finished profile UI

--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -1,15 +1,18 @@
 <template>
-    <div class="w-full h-[90vh] grid grid-cols-3 gap-5">
-        <div class="h-full flex flex-col justify-center items-center">
+    <div class="w-full h-[90vh] grid grid-cols-2 gap-x-24 justify-center">
+        <div class="h-full flex flex-col gap-y-8 justify-center items-center">
             <img src="/Images/Camera/canon-ae1.jpg" class="w-4/5 rounded-[50%] aspect-square inline-block border-2"/>
-            <p class="self-start text-2xl ml-5 mt-5">Eav Long Sok</p>
-            <button class="bg-white w-full p-3 mt-5 text-center rounded-lg border-2 border-gray-500 ml-5 text-xl hover:bg-blue-100">Edit Profile</button>
+            <p class="text-2xl font-bold">Eav Long Sok</p>
+            <button class="bg-white w-4/5 p-3 mt-5 text-center rounded-lg border-2 border-gray-500 ml-5 text-xl hover:bg-blue-100">Edit Profile</button>
         </div>
-        <div class="bg-red-100 h-full">
-
-        </div>
-        <div class="bg-green-100 h-full">
-
+        <div class="h-full flex flex-col indent-10 min-w-[25rem] justify-center text-xl gap-y-5">
+            <p><span class="font-bold capitalize">Name:</span> Eav Long Sok</p>
+            <p><span class="font-bold capitalize">Email:</span>esok@paragoniu.edu.kh</p>
+            <p><span class="font-bold capitalize">Date Of Birth:</span> 2000-01-01</p>
+            <p><span class="font-bold capitalize">Phone Number:</span> 011234531</p>
+            <p><span class="font-bold capitalize">Verified:</span> Yes</p>
+            <p><span class="font-bold capitalize">Store Name:</span> Asus</p>
+            <p><span class="font-bold capitalize">Store Information:</span> Asus company description</p>
         </div>
     </div>
 </template>
